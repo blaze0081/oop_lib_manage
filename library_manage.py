54fe5144ff3f07273313bae3c9cfc514198d0567
blaze0081 <68160298+blaze0081@users.noreply.github.com>
2023-06-01 00:26:28 +0530

    Update library_manage.py

--- library_manage.py
+++ library_manage.py
@@ -2,11 +2,20 @@ class Library:
 
     def __init__(self, game):
         self.game = game
+        self.lendDict = {}
 
     def show(self):
         print("Following games are available")
         for game in self.game:
             print(game)
+
+    def lend(self, user, book):
+        if book not in self.lendDict.keys():
+            self.lendDict.update({book:user})
+            print("You can now borrow")
+        else:
+            print(f"Book has been issued to {self.lendDict[book]}. You can't Borrow")
+
 
     def add(self):
         print("Enter the game")
@@ -18,11 +27,25 @@ class Library:
         if a==1:
             game.show()
             Library.initilization()
+
         elif a==2:
+            print("Correspondinbg Number to book you want to borrow")
+            game.show()
+            name=input("Book you want to borrow:")
+            user=input("Enter your name: ")
+            game.lend(user, name)
+            Library.initilization()
+
+        elif a==3:
             game.add()
             Library.initilization()
+
+        elif a==4:
+            pass
+
         elif a==5:
             return 0    
+        
         else:
             print('Unknown Command')
 
